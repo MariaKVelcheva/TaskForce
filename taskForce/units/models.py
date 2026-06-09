69b461ee72f635818f291e85a922779daef242c9
MariaKVelcheva <maria.k.velcheva@gmail.com>
2026-06-09 14:52:23 +0200

    added: unit models

--- taskForce/units/models.py
+++ taskForce/units/models.py
@@ -1,5 +1,75 @@
+import uuid
+
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Unit(models.Model):
-    pass
+    users = models.ManyToManyField(
+        to=User,
+        related_name='units',
+        verbose_name=_('users'),
+    )
+
+    name = models.CharField(
+        verbose_name=_('name'),
+        max_length=55,
+    )
+
+    invite_code = models.UUIDField(
+        default=uuid.uuid4,
+        verbose_name=_('invite code'),
+        unique=True,
+        editable=False,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('unit')
+        verbose_name_plural = _('units')
+
+
+class Membership(models.Model):
+    ROLES = (
+        ("commander", _("Commander")),
+        ("operative", _("Operative")),
+    )
+
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        verbose_name=_('user'),
+        related_name='memberships',
+    )
+
+    role = models.CharField(
+        verbose_name=_('role'),
+        choices=ROLES,
+        max_length=55,
+        default='operative',
+    )
+
+    unit = models.ForeignKey(
+        to=Unit,
+        on_delete=models.CASCADE,
+        verbose_name=_('unit'),
+        related_name='memberships',
+    )
+
+    def __str__(self):
+        return f'{self.user}: {self.role} of {self.unit}'
+
+    class Meta:
+        verbose_name = _('membership')
+        verbose_name_plural = _('memberships')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'unit'],
+                name='unique_membership',
+            )
+        ]
--- taskForce/zones/models.py
+++ taskForce/zones/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 
 
@@ -7,3 +8,26 @@ class Zone(models.Model):
         max_length=100,
         verbose_name=_('Name'),
     )
+
+    slug = models.SlugField(
+        max_length=100,
+        verbose_name=_('Slug'),
+        editable=False,
+        unique=True,
+        null=True,
+        blank=True,
+    )
+
+    scene_position = models.JSONField(
+        default=dict,
+        verbose_name=_('Scene position'),
+        blank=True,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
