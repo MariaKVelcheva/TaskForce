97eedd10ff35611dfab72d6471f91c1d302786e0
MariaKVelcheva <maria.k.velcheva@gmail.com>
2026-07-15 14:43:10 +0200

    added: zone views

--- taskForce/zones/urls.py
+++ taskForce/zones/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from taskForce.zones import views
 
 urlpatterns = [
-
+    path("all-zones/", views.zone_scene_data, name="zone-scene"),
+    path("<slug:slug>/details/", views.ZoneDetailView.as_view(), name="details-zone"),
 ]
--- taskForce/zones/views.py
+++ taskForce/zones/views.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q, Count
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import DetailView
 
-# Create your views here.
+from taskForce.tasks.models import Task
+from taskForce.units.models import Membership, Unit
+from taskForce.zones.models import Zone
+
+
+@login_required
+def zone_scene_data(request):
+    user = request.user
+    unit_ids = Unit.objects.filter(memberships__user=user).values_list('id', flat=True)
+    query = Q(tasks__is_done=False) & (Q(tasks__user=user) | Q(tasks__unit__in=unit_ids))
+    zones = Zone.objects.annotate(open_tasks=Count("tasks", filter=query, distinct=True))
+
+    return JsonResponse({
+        "zones": [
+            {
+                "slug": zone.slug,
+                "name": zone.name,
+                "scene_position": zone.scene_position,
+                "open_tasks": zone.open_tasks,
+                "details_url": reverse("details-zone", kwargs={"slug": zone.slug}),
+            }
+            for zone in zones
+        ]
+    })
+
+
+class ZoneDetailView(DetailView):
+    pass
